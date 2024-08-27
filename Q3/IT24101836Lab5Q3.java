5f62e684c1f30b386804c1d935c11d905a0ada07
Sajeev-1 <it24101836@my.sliit.lk>
2024-08-27 20:52:48 +0530

    Update IT24101836Lab5Q3.java

--- Q3/IT24101836Lab5Q3.java
+++ Q3/IT24101836Lab5Q3.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 public class IT24101836Lab5Q3 {
 
-              public static void main(String[] args) {
+	public static void main(String[] args) {
    	Scanner input = new Scanner(System.in);
 
 		//Declare 
